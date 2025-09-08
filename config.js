5d678fd5652c2b8c99d261cb5bc5a9dd3c4c931a
Ahmed Sayed <31688712+iilemoz@users.noreply.github.com>
2025-09-08 10:10:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
-// Configuration file for Lemo's Profile
-// This file should be added to .gitignore to keep sensitive data secure
-
 const config = {
-  // Firebase Configuration
   firebase: {
     apiKey: "AIzaSyA8FxTP0_ue_tnE2hF9kJzHdlC575ePo-I",
     authDomain: "lemo-profile-stories.firebaseapp.com",
@@ -10,6 +6,11 @@ const config = {
     storageBucket: "lemo-profile-stories.firebasestorage.app",
     messagingSenderId: "1039102929805",
     appId: "1:1039102929805:web:2d45c7febaecab3e37abc4"
+  },
+  
+  // Admin Configuration
+  admin: {
+    password: "lemo265339"
   }
 };
 
@@ -17,4 +18,3 @@ const config = {
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = config;
 }
-
